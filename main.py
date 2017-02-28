3dea189ca90ec9f29e39a8835ba5adcf243b8fdc
Casey Clayton <claytoncasey01@gmail.com>
2017-02-27 19:20:30 -0600

    Added extensions list

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ import os
 rootdir = sys.argv[1]
 fd = FaceDetector()
 
+# List of extensions so search for
+extensions = ["jpg", "png", "gif", "jpeg"]
+
 # Loop through all directories and subdirectories
 # in the path entered
 for subdir, dirs, files in os.walk(rootdir):
     # Loop through all files
     for f in files:
         # Check if they are images
-        if ".jpg" in f:
-
-            try:
-                detected = fd.detect(os.path.join(subdir, f))
-                os.remove(os.path.join(subdir, f))
-
-            except:
-                print("Error on file" + os.path.join(subdir, f) + " removing...")
-                os.remove(os.path.join(subdir, f))
+        for extension in extensions:
+            if extension in f:
+                try:
+                    detected = fd.detect(os.path.join(subdir, f))
+                    os.remove(os.path.join(subdir, f))
+                except:
+                    print("Error on file" + os.path.join(subdir, f) + " removing...")
+                    os.remove(os.path.join(subdir, f))
