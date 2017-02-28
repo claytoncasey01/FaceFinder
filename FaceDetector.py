fd279cedafdabab7be0323ca2718b9b8fd13611f
Casey Clayton <claytoncasey01@gmail.com>
2017-02-27 19:01:21 -0600

    Updates to face extraction

--- FaceDetector.py
+++ FaceDetector.py
@@ -10,28 +10,21 @@ class FaceDetector:
         cascade = cv2.CascadeClassifier("/home/casey/FaceFinder/haarcascade_frontalface_alt.xml")
         faces = cascade.detectMultiScale(img, 1.3, 4, cv2.CASCADE_SCALE_IMAGE, (20, 20))
 
-        self.extract_faces(img, faces)
+        self.extract_faces(img, faces, path)
 
         # Check if we found any faces
         if len(faces) == 0:
             return False
 
-            # if self.crop_faces:
-            #   print("Made it")
-            #   self.extract_faces(img, faces)
-
         return True
 
-    def extract_faces(self, img, faces):
-        print("Here")
+    def extract_faces(self, img, faces, path):
         for f in faces:
             x, y, w, h = [v for v in f]
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255))
 
             sub_face = img[y:y + h, x:x + w]
-            face_file_name = "ace_" + str(y) + ".jpg"
+            face_file_name = path + "face_" + str(y) + ".jpg"
             cv2.imwrite(face_file_name, sub_face)
-
-        cv2.imshow("Window", img)
 
         return
